da2b70213d4750b8b7ca47261bfe48b6d3d577c3
Kernaonet.A <akernaonet@gmail.com>
2021-11-24 17:00:34 +0100

    debut js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#instruction').click(function(){
+        $('#cookiesModal').style.display = 'block';
+    })
+    $('#closeModal').click(function() {
+        $('#cookiesModal').hide();
+    })
+});
